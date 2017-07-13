dacf5dbc6b0904a4c214a808135695902a3caafe
Paul Chote <pchote@users.noreply.github.com>
2017-07-13 14:55:42 +0000

    Disable microphone when no clients are connected.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ node:
 	mkdir -p build/var/www/microphone-server
 	mkdir -p build/lib/systemd/system/
 	cp -r DEBIAN build
-	cp -r node_modules microphone.js server.js build/var/www/microphone-server
+	cp -r node_modules server.js build/var/www/microphone-server
 	cp microphone-server.service build/lib/systemd/system/
-	fakeroot dpkg-deb -b build "microphone_server_1.0_all.deb"
+	fakeroot dpkg-deb -b build "microphone_server_1.1_all.deb"
 	rm -rf build
--- server.js
+++ server.js
@@ -1,27 +1,45 @@
+var config = {
+  port: process.argv[2],
+  device: process.argv[3],
+  sampleRate: 22050,
+  bufferTime: 100000
+};
+
 var BinaryServer = require('binaryjs').BinaryServer;
-var mic = require('./microphone.js');
+var spawn = require('child_process').spawn
 
+var server = BinaryServer({port: config.port});
 var clients = [];
+var arecord = null;
 
-var server = BinaryServer({port: process.argv[2]});
+var startStreaming = function() {
+  var firstChunk = true;
+  arecord = spawn('arecord', ['-D', config.device, '-f', 'FLOAT_LE', '--buffer-time=' + config.bufferTime, '-r', config.sampleRate]);
+  arecord.stdout.on('data', function(data) {
+    if (firstChunk) {
+      firstChunk = false;
+      return;
+    }
+
+    for (var i = clients.length - 1; i >= 0; i--) {
+      try {
+        clients[i].send(data);
+      }
+      catch (err) {
+        clients.splice(i, 1);
+        if (clients.length == 0 && arecord != null) {
+          arecord.kill();
+          arecord = null;
+        }
+      }
+    }
+  });
+}
 
 server.on('connection', function(client) {
   clients.push(client);
+  if (arecord == null)
+    startStreaming();
 });
 
-mic.startCapture({
-  device: process.argv[3],
-  sampleRate: 22050,
-  bufferTime: 500000
-});
 
-mic.audioStream.on('data', function(data) {
-  for (var i = clients.length - 1; i >= 0; i--) {
-    try {
-      clients[i].send(data);
-    }
-    catch (err) {
-      clients.splice(i, 1);
-    }
-  }
-});
